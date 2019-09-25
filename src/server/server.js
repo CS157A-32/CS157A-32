44549e8c952e4dd15ed3409ed8f472cfd5c729bc
Sandro Sallenbach <hcl.sandro@hotmail.de>
2019-09-25 14:33:21 -0700

    added mySQL connection

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,26 @@
 const express = require("express");
+const mysql = require('mysql');
 
 const app = express();
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'xyz',
+  database: 'cs157a'
+
+});
+
+connection.connect(function(err){
+  (err)? console.log(err): console.log(connection);
+
+});
+
 app.get("/api/schools", (req, res) => {
-  const schools = [
-    { id: 1, name: "SJSU", conference: "MWest" },
-    { id: 2, name: "Stanford", conference: "Pac-10" },
-    { id: 3, name: "Texas", conference: "Big-12" }
-  ];
-  res.json(schools);
+
+  connection.query('SELECT * FROM schools', function(err, data){
+    (err)?res.send(err):res.json(data);
+  });
 });
 
 const port = 5000;
