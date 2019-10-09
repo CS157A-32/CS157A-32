bfa3696896dcb5f6072ed012f930a86efe08ac98
Sandro Sallenbach <hcl.sandro@hotmail.de>
2019-10-09 01:26:19 -0700

    started frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from "react";
 import "./App.css";
-import Schools from "./components/Schools";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
+import NavBar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/landing_page/Landing";
+import searchResultOverview from "./components/search_results/SearchResults";
+
 
 function App() {
   return (
-    <div className="App">
-      <Schools />
-    </div>
+    <Provider store={store}>
+
+      <Router>
+
+        <div className="App">
+          <NavBar />
+          <Route exact path="/" component={Landing} />
+          <Route
+                exact
+                path="/searchResultOverview"
+                component={searchResultOverview}
+              />
+          <Footer />
+
+        </div>
+      </Router>
+      </Provider>
+
   );
 }
 
