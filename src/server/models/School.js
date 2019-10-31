fb921d485624056492bece98d8c4144598f142fe
Chrisrdouglas <chrisr.douglas@yahoo.com>
2019-10-31 13:19:13 -0700

    School stuff discussed on slack

---
+++ src/server/models/School.js
@@ -0,0 +1,29 @@
+const Sequelize = require("sequelize");
+const db = requir("../db/db");
+module.exports = db.Sequelize.define(
+    "school",
+    {
+        name: {
+            type: Sequelize.STRING,
+            primaryKey: true,
+            allowNull: false
+          },
+        conference: {
+            type: Sequelize.STRING,
+            primaryKey: true,
+            allowNull: false
+          },
+          attendance: {
+              type: Sequelize.INTEGER,
+          },
+          tuition: {
+              type: Sequelize.INTEGER,
+          },
+          mascot: {
+              type: Sequelize.STRING,
+          }
+    },
+    {
+        timestamps: false
+    }
+)
