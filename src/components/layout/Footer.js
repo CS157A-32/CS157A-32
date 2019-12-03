ee64a2373672b20954bc756392594e780d82306b
Sandro Sallenbach <hcl.sandro@hotmail.de>
2019-12-02 21:19:41 -0800

    update Admin panel and NBA backend

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
               className="footerMenuLink"
               style={{ color: "white" }}
             >
-              Admin Login
+              Admin Panel
             </Link>
           </Grid>
         </Grid>
@@ -33,68 +33,3 @@ function Footer() {
 }
 
 export default Footer;
-
-// import React, { Component } from "react";
-// import "./Footer.css";
-// import { Link } from "react-router-dom";
-// import Grid from "@material-ui/core/Grid";
-// import { connect } from "react-redux";
-// import { logoutUser } from "../../actions/authActions";
-// import Button from "@material-ui/core/Button";
-
-// class Footer extends Component {
-//   constructor() {
-//     super();
-//     this.state = {};
-//     this.handleLogout = this.handleLogout.bind(this);
-//   }
-
-//   handleLogout() {
-//     this.props.logoutUser();
-//   }
-
-//   render() {
-//     return (
-//       <footer id="footer">
-//         <Grid container alignItems="center" justify="space-evenly">
-//           <Grid container lg={7} alignItems="center" justify="space-evenly">
-//             <Grid className="footerLayout" item>
-//               <Link
-//                 to="/aboutus"
-//                 className="footerMenuLink"
-//                 style={{ color: "white" }}
-//               >
-//                 About Us
-//               </Link>
-//             </Grid>
-//             <Grid className="footerLayout" item>
-//               {this.props.auth.isAuthenticated ? (
-//                 <Button
-//                   className="buttonSearch"
-//                   primary
-//                   onClick={this.handleLogout()}
-//                 >
-//                   Logout Admin
-//                 </Button>
-//               ) : (
-//                 <Link
-//                   to="/login"
-//                   className="footerMenuLink"
-//                   style={{ color: "white" }}
-//                 >
-//                   Admin Login
-//                 </Link>
-//               )}
-//             </Grid>
-//           </Grid>
-//         </Grid>
-//       </footer>
-//     );
-//   }
-// }
-
-// let mapStateToProps = state => ({
-//   auth: state.auth
-// });
-
-// export default connect(mapStateToProps, { logoutUser })(Footer);
--- src/server/routes/NbaPlayers.js
+++ src/server/routes/NbaPlayers.js
@@ -23,16 +23,13 @@ nbaplayers.get("/byTeam", (req, res) => {
 });
 
 nbaplayers.post("/addPlayer", (req, res) => {
-  console.log(req.body);
   let newPlayerId;
   pool.query(
     `INSERT INTO nbaplayers (name, college, position, nbasince, team, salary)
         VALUES ("${req.body.name}", "${req.body.college}", "${req.body.position}", ${req.body.yeardrafted}, "${req.body.team}", ${req.body.salary})`,
     (err, result) => {
       if (err) throw err;
-      console.log(result);
       newPlayerId = result.insertId;
-      console.log(result.insertId);
       pool.query(
         `INSERT INTO playedfor (pid, schoolname, conference)
           VALUES (${newPlayerId}, "${req.body.college}", "${req.body.conference}")`,
@@ -46,21 +43,10 @@ nbaplayers.post("/addPlayer", (req, res) => {
 });
 
 nbaplayers.post("/deletePlayer", (req, res) => {
-  console.log(req.body);
-  let playerId;
-  // pool.query(
-  //   `SELECT id FROM nbaplayers WHERE name = "${req.body.name}" AND team = "${req.body.team}"`,
-  //   (err, result) => {
-  //     if (err) throw err;
-  //     console.log(result);
-  //     console.log(result[0].id);
-  //     playerId = result[0].id;
-  //     console.log(playerId);
   pool.query(
     `DELETE FROM nbaplayers WHERE id = ${req.body.playerId}`,
     (err, result) => {
       if (err) throw err;
-      console.log(result.affectedRows);
       pool.query(
         `DELETE FROM playedfor WHERE pid = ${req.body.playerId}`,
         (err, result) => {
@@ -70,8 +56,6 @@ nbaplayers.post("/deletePlayer", (req, res) => {
       );
     }
   );
-  // }
-  // );
 });
 
 nbaplayers.post("/editPlayer", (req, res) => {
